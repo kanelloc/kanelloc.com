f39c55dd8b75400f05072631d6c6dd3b3b2b46fb
Babis Kanellopoulos <babis@relive.cc>
2022-11-25 23:30:38 +0200

    Mod: links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Container, Heading } from '@chakra-ui/react';
+import { Button, Container, Flex, Heading, IconButton, Link } from '@chakra-ui/react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/all';
 
 const Home = () => {
   return (
     <Container maxWidth="1024px">
-      <Heading>Hi ! 👋</Heading>
+      <Flex alignItems="flex-start" justifyContent="center" flexDirection="column">
+        <Heading>Hi ! 👋</Heading>
+        <Flex flexDirection="row" alignItems="center">
+          <Button marginY="24px">Say hello!</Button>
+          <Link href="https://github.com/kanelloc" isExternal marginX="16px">
+            <AiFillGithub size="32px" />
+          </Link>
+          <Link href="https://www.linkedin.com/in/charkanellopoulos/" isExternal>
+            <AiFillLinkedin size="32px" />
+          </Link>
+        </Flex>
+      </Flex>
     </Container>
   );
 };
